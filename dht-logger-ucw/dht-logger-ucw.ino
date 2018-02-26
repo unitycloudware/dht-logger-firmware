d3a99a3d4bcf1227fc900dbf32f09f46d6c73001
Tomas Hrdlicka <tomas@hrdlicka.co.uk>
2018-02-26 18:13:44 +0100

    Adding skeleton project for dht-logger-ucw

---
+++ dht-logger-ucw/dht-logger-ucw.ino
@@ -0,0 +1,15 @@
+/* 
+  DHT Logger Firmware - Data logger for DHTx sensors
+  Copyright 2018 Unity{Cloud}Ware - UCW Industries Ltd. All rights reserved.
+ */
+
+#include <SPI.h>
+#include <WiFi101.h>
+#include <DHT.h>
+
+void setup() {
+}
+
+void loop() {
+  delay(1000);
+}
